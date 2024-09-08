3d5f379749a89943fed64f361d007c90b8943721
MIDZY <itmidnal0212@gmail.com>
2024-09-08 14:09:14 +0000

    컴백앞둔 돌이

---
+++ timer.js
@@ -0,0 +1,38 @@
+export default {
+  "2024-09-12 18:00:00": {
+    track: "BORN TO BE",
+    content: "내 생에 복날은,",
+    member: "ITZY",
+  },
+
+  "2024-09-29 00:00:00": {
+    track: "BORN TO BE",
+    content: "영동대로 K-POP 콘서트",
+    member: "ITZY",
+  },
+
+  "2024-10-05 00:00:00": {
+    track: "BORN TO BE",
+    content: "K-link 페스티벌",
+    member: "ITZY",
+  },
+
+  "2024-10-13 00:00:00": {
+    track: "BORN TO BE",
+    content: "인기가요 in 도쿄",
+    member: "ITZY",
+  },
+
+  "2024-11-02 17:00:00": {
+    track: "BORN TO BE",
+    content: "3rd 팬미팅",
+    member: "ITZY",
+  },
+
+  "2024-12-14 00:00:00": {
+    track: "BORN TO BE",
+    content: "뮤직뱅크 in 일본",
+    member: "ITZY",
+  },
+
+};
