2dc995d19692ab8427a7192c009adca8da00abc2
MIDZY <itmidnal0212@gmail.com>
2024-09-12 16:37:40 +0000

    컴백앞둔 돌이

--- timer.js
+++ timer.js
@@ -1,44 +1,116 @@
 export default {
-  "2024-09-09 00:00:00": {
-    track: "BORN TO BE",
-    content: "테스트",
+
+  "2024-09-13 00:00:00": {
+    track: "GOLD",
+    content: "TIME TABLE",
     member: "ITZY",
   },
 
-  "2024-09-12 18:00:00": {
-    track: "BORN TO BE",
-    content: "내 생에 복날은,",
+  "2024-09-30 00:00:00": {
+    track: "GOLD",
+    content: "TRAILER",
     member: "ITZY",
   },
 
-  "2024-09-29 00:00:00": {
-    track: "BORN TO BE",
-    content: "영동대로 K-POP 콘서트",
+  "2024-10-01 00:00:00": {
+    track: "GOLD",
+    content: "CONCEPT PHOTO #1",
     member: "ITZY",
   },
 
-  "2024-10-05 00:00:00": {
-    track: "BORN TO BE",
-    content: "K-link 페스티벌",
+  "2024-10-02 00:00:00": {
+    track: "GOLD",
+    content: "CONCEPT PHOTO #2",
     member: "ITZY",
   },
 
-  "2024-10-13 00:00:00": {
-    track: "BORN TO BE",
-    content: "인기가요 in 도쿄",
+  "2024-10-03 00:00:00": {
+    track: "GOLD",
+    content: "CONCEPT PHOTO #3",
+    member: "ITZY",
+  },
+
+  "2024-10-04 00:00:00": {
+    track: "GOLD",
+    content: "CONCEPT PHOTO #4",
+    member: "ITZY",
+  },
+
+  "2024-10-07 00:00:00": {
+    track: "GOLD",
+    content: "IMAGINARY TRACK",
+    member: "ITZY",
+  },
+
+  "2024-10-09 00:00:00": {
+    track: "GOLD",
+    content: "M/V TEASER #1",
+    member: "ITZY",
+  },
+
+  "2024-10-11 00:00:00": {
+    track: "GOLD",
+    content: "M/V TEASER #2",
+    member: "ITZY",
+  },
+
+  "2024-10-14 00:00:00": {
+    track: "GOLD",
+    content: "ALBUM SPOILER",
+    member: "ITZY",
+  },
+
+  "2024-10-15 00:00:00": {
+    track: "GOLD",
+    content: "\"GOLD\" PHOTO",
+    member: "ITZY",
+  },
+
+  "2024-10-15 17:00:00": {
+    track: "GOLD",
+    content: "COUNTDOWN LIVE",
+    member: "ITZY",
+  },
+
+  "2024-10-15 18:00:00": {
+    track: "GOLD",
+    content: "\"GOLD\" RELEASE",
+    member: "ITZY",
+  },
+
+  "2024-10-23 00:00:00": {
+    track: "GOLD",
+    content: "\"Imaginary Friend\" M/V TEASER #1",
+    member: "ITZY",
+  },
+
+  "2024-10-25 00:00:00": {
+    track: "GOLD",
+    content: "\"Imaginary Friend\" M/V TEASER #2",
+    member: "ITZY",
+  },
+
+  "2024-10-25 13:00:00": {
+    track: "GOLD",
+    content: "\"US PHYSICAL\" RELEASE",
+    member: "ITZY",
+  },
+
+  "2024-10-27 00:00:00": {
+    track: "GOLD",
+    content: "\"Imaginary Friend\" PHOTO",
+    member: "ITZY",
+  },
+
+  "2024-10-28 00:00:00": {
+    track: "GOLD",
+    content: "\"Imaginary Friend\" M/V",
     member: "ITZY",
   },
 
   "2024-11-02 17:00:00": {
-    track: "BORN TO BE",
-    content: "3rd 팬미팅",
+    track: "GOLD",
+    content: "ITZY THE 3RD FANMEETING",
     member: "ITZY",
   },
-
-  "2024-12-14 00:00:00": {
-    track: "BORN TO BE",
-    content: "뮤직뱅크 in 일본",
-    member: "ITZY",
-  },
-
 };
